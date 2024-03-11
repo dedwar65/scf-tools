1b49a9ae721e78c922773b318743c0b3e14dd305
alanlujan91 <alanlujan91@gmail.com>
2024-03-11 12:54:50 -0400

    update metadata

--- src/scf_tools/__init__.py
+++ src/scf_tools/__init__.py
@@ -4,7 +4,6 @@ Copyright (c) 2024 Alan Lujan. All rights reserved.
 scf-tools: Tools to download and analyze the Survey of Consumer Finances.
 """
 
-
 from __future__ import annotations
 
 from ._version import version as __version__
