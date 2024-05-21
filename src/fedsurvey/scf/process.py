cb9ddf075a1956550154a913cf774ef1681a278b
Alan Lujan <alanlujan91@gmail.com>
2024-05-21 14:33:09 -0400

    remove mypy and spell check

--- src/fedsurvey/scf/process.py
+++ src/fedsurvey/scf/process.py
@@ -8,7 +8,7 @@ import pandas as pd
 current_dir = Path(__file__).resolve().parent
 
 # Load the data from a .dta file into a DataFrame
-df = pd.read_stata(current_dir / "data/_raw/scf_merged.dta")
+scf_merged = pd.read_stata(current_dir / "data/_raw/scf_merged.dta")
 
 # Define the mapping dictionaries
 mappings = {
@@ -47,47 +47,49 @@ mappings = {
     },
 }
 
-df_new = pd.DataFrame()
+scf_merged_labeled = pd.DataFrame()
 
 # Apply all mappings and transformations directly on the original DataFrame
 for column, mapping in mappings.items():
-    df_new[f"{column}_lbl"] = df[column].map(mapping)
+    scf_merged_labeled[f"{column}_lbl"] = scf_merged[column].map(mapping)
 
 # Define labels for age groups
 labels = [f"({i+1}-{i+5}]" for i in range(20, 95, 5)]
 
 # Create a new column 'age_lbl' with age groups
-df_new["age_lbl"] = pd.cut(df["age"], bins=range(20, 100, 5), right=True, labels=labels)
-
-# Calculate 'equitfin', 'fininc', 'finmill', 'fillthou', 'incomemill', 'incomethou' in one line
-df_new = df_new.assign(
-    equityfin=df["equity"]
-    .div(df["fin"], fill_value=np.nan)
-    .replace([np.inf, -np.inf], np.nan),
-    finincome=df["fin"]
-    .div(df["income"], fill_value=np.nan)
-    .replace([np.inf, -np.inf], np.nan),
-    finmill=df["fin"].div(1_000_000, fill_value=np.nan),
-    fillthou=df["fin"].div(1_000, fill_value=np.nan),
-    incomemill=df["income"].div(1_000_000, fill_value=np.nan),
-    incomethou=df["income"].div(1_000, fill_value=np.nan),
+scf_merged_labeled["age_lbl"] = pd.cut(
+    scf_merged["age"], bins=range(20, 100, 5), right=True, labels=labels
 )
 
-df_new["findeciles"] = (
-    df.groupby("year")["fin"].transform(
+# Calculate 'equitfin', 'fininc', 'finmill', 'fillthou', 'incomemill', 'incomethou' in one line
+scf_merged_labeled = scf_merged_labeled.assign(
+    equityfin=scf_merged["equity"]
+    .div(scf_merged["fin"], fill_value=np.nan)
+    .replace([np.inf, -np.inf], np.nan),
+    finincome=scf_merged["fin"]
+    .div(scf_merged["income"], fill_value=np.nan)
+    .replace([np.inf, -np.inf], np.nan),
+    finmill=scf_merged["fin"].div(1_000_000, fill_value=np.nan),
+    fillthou=scf_merged["fin"].div(1_000, fill_value=np.nan),
+    incomemill=scf_merged["income"].div(1_000_000, fill_value=np.nan),
+    incomethou=scf_merged["income"].div(1_000, fill_value=np.nan),
+)
+
+scf_merged_labeled["findeciles"] = (
+    scf_merged.groupby("year")["fin"].transform(
         lambda x: pd.qcut(x, q=10, duplicates="drop", labels=False),
     )
     / 10
 )
 
 
-df_new.replace([np.inf, -np.inf], np.nan, inplace=True)
+scf_merged_labeled = scf_merged_labeled.replace([np.inf, -np.inf], np.nan)
 
-df = pd.concat([df, df_new], axis=1)
+scf_merged = pd.concat([scf_merged, scf_merged_labeled], axis=1)
 
-df = df.filter(
+scf_merged = scf_merged.filter(
     regex="age|race|hhsex|edcl|married|lf|fin|inc|equity|networth|asset|year|wgt|savres",
 )
 
 # Save the processed DataFrame back to a .dta file
-df.to_stata(current_dir / "data/scf_processed.dta", write_index=False)
+scf_merged.to_stata(current_dir / "data/scf_processed.dta", write_index=False)
